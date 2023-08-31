44fbabc9046c2405062e1ea288f367ac092e6698
Deva <deva.palasingam@gmail.com>
2023-08-31 13:58:31 -0500

    adding some styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts}"],
   theme: {
     extend: {},
+    colors: {
+      pizzaDarkGreen: "#264653",
+      pizzaLightGreen: "#2a9d8f",
+      pizzaYellow: "#e9c46a",
+      pizzaPeach: "#f4a261",
+      pizzaOrange: "#e76f51",
+      pizzaRed: "#e63946",
+    },
   },
   plugins: [],
 };
