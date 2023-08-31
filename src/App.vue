5e5f7624f66fc917fba88a5229e1db14c9efd190
Deva <deva.palasingam@gmail.com>
2023-08-31 00:26:47 -0500

    working on navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
+  <Navbar />
   <div class="text-3xl font-bold underline">Hello World</div>
 </template>
 
